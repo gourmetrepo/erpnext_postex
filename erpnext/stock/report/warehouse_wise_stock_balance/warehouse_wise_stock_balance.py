c50085e52a99e32fba5dc47b7465d9aeff95faf3
Shoaib Rehmat <86551638+shoaibrehmat88@users.noreply.github.com>
2024-09-06 17:16:51 +0500

    warehouse name change in report

--- erpnext/stock/report/warehouse_wise_stock_balance/warehouse_wise_stock_balance.py
+++ erpnext/stock/report/warehouse_wise_stock_balance/warehouse_wise_stock_balance.py
@@ -49,7 +49,7 @@ def get_warehouses(report_filters: StockBalanceFilter):
 
 	return frappe.get_all(
 		"Warehouse",
-		fields=["name", "parent_warehouse", "is_group", "disabled"],
+		fields=["warehouse_name AS name", "parent_warehouse", "is_group", "disabled"],
 		filters=filters,
 		order_by="lft",
 	)
